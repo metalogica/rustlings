5a696d3d4816c8f522d29c817a776fc04e5d566f
richard jarram <rjarram@me.com>
2021-01-22 12:21:15 -0500

    refactored struct challenge

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -4,12 +4,12 @@
 
 // I AM NOT DONE
 
-struct ColorClassicStruct<'a> {
-  name: &'a str,
-  hex: &'a str
+struct ColorClassicStruct {
+  name: String,
+  hex: String
 }
 
-struct ColorTupleStruct<'a>(&'a str, &'a str);
+struct ColorTupleStruct(String, String);
 
 #[derive(Debug)]
 struct UnitStruct;
@@ -21,7 +21,7 @@ mod tests {
     #[test]
     fn classic_c_structs() {
         // TODO: Instantiate a classic c struct!
-        let green = ColorClassicStruct { name: "green", hex: "#00FF00" };
+        let green = ColorClassicStruct { name: String::from("green"), hex: String::from("#00FF00") };
 
         assert_eq!(green.name, "green");
         assert_eq!(green.hex, "#00FF00");
@@ -30,7 +30,7 @@ mod tests {
     #[test]
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct!
-        let green = ColorTupleStruct("green", "#00FF00");
+        let green = ColorTupleStruct(String::from("green"), String::from("#00FF00"));
 
         assert_eq!(green.0, "green");
         assert_eq!(green.1, "#00FF00");
