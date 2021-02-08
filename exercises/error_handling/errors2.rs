dba95381ccababf454230ec6d65319be72c1cd33
richard jarram <rjarram@me.com>
2021-02-08 10:38:13 -0500

    solved err2

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -23,7 +23,12 @@ use std::num::ParseIntError;
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
+    let qty = item_quantity.parse::<i32>()?;
+
+    // let mut qty = match qty {
+    //   Ok(q) => q,
+    //   Err(e) => return Err(e)
+    // };
 
     Ok(qty * cost_per_item + processing_fee)
 }
