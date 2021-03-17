c9b435279966fbdffa66ea89c2f2f2862a1e3597
richard jarram <rjarram@me.com>
2021-03-16 21:57:45 -0700

    completed move_semantics1

--- exercises/traits/traits1.rs
+++ exercises/traits/traits1.rs
@@ -8,8 +8,6 @@
 // which appends "Bar" to any object
 // implementing this trait.
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> String;
 }
@@ -17,7 +15,7 @@ trait AppendBar {
 impl AppendBar for String {
     //Add your code here
     fn append_bar(self) -> String {
-      self + &String::from("Bar")
+      self + String::from("Bar")
     }
 }
 
