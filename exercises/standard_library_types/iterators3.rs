59b382064b2b43b5f694ab81fdf4ccf6816d71d8
richard jarram <rjarram@me.com>
2021-04-22 15:54:08 -0700

    completed test 3/4 in iterators3.

--- exercises/standard_library_types/iterators3.rs
+++ exercises/standard_library_types/iterators3.rs
@@ -7,6 +7,7 @@
 // Execute `rustlings hint iterators3` to get some hints!
 // Have fun :-)
 
+// hint: https://doc.rust-lang.org/std/iter/trait.Iterator.html
 // I AM NOT DONE
 
 #[derive(Debug, PartialEq, Eq)]
@@ -24,7 +25,19 @@ pub struct NotDivisibleError {
 // This function should calculate `a` divided by `b` if `a` is
 // evenly divisible by b.
 // Otherwise, it should return a suitable error.
-pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {}
+pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
+  // check to see if a % b == 0
+  let even = a % b == 0;
+  // if so, then return it
+  if even {
+    return Result::Ok(a/b)
+  }
+  // else return an error
+  Err(DivisionError::NotDivisible(NotDivisibleError {
+    dividend: a,
+    divisor: b
+  }))
+}
 
 #[cfg(test)]
 mod tests {
@@ -58,12 +71,11 @@ mod tests {
     }
 
     // Iterator exercises using your `divide` function
-    /*
     #[test]
     fn result_with_list() {
         let numbers = vec![27, 297, 38502, 81];
         let division_results = numbers.into_iter().map(|n| divide(n, 27));
-        let x //... Fill in here!
+        let x = division_results.collect::<Result<Vec<_>, DivisionError>>();
         assert_eq!(format!("{:?}", x), "Ok([1, 11, 1426, 3])");
     }
 
@@ -71,8 +83,7 @@ mod tests {
     fn list_of_results() {
         let numbers = vec![27, 297, 38502, 81];
         let division_results = numbers.into_iter().map(|n| divide(n, 27));
-        let x //... Fill in here!
+        let x = vec![1,2,3];
         assert_eq!(format!("{:?}", x), "[Ok(1), Ok(11), Ok(1426), Ok(3)]");
     }
-    */
 }
