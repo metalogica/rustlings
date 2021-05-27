9618ad746e80fba22d86207973e0b05d6f7897c0
richard jarram <rjarram@me.com>
2021-05-26 19:41:36 -0700

    edited comments

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -18,6 +18,7 @@ struct JobStatus {
 
 fn main() {
     let new_job = JobStatus { jobs_completed: 0 };
+    // TO DO: Change this line
     let status = Arc::new(Mutex::new(new_job));
     let status_shared = Arc::clone(&status);
     // spawn a new thread that will run a for-loop code block 10 times
@@ -25,11 +26,13 @@ fn main() {
         // in this code block, the new thread will sleep 250ms, and then increment the counter in JobStatus
         for _ in 0..10 {
             thread::sleep(Duration::from_millis(250));
+            // TO DO: Change this line
             status_shared.lock().unwrap().jobs_completed += 1;
         }
     });
     // whilst the spawned thread has begun running, we will peek into the JobStatus on 500ms intervals to check the counter
     // if the counter has reached 10, we stop running the code block below.
+    // TO DO: Change this line
     while status.lock().unwrap().jobs_completed < 10 {
         println!("waiting... Current Count: {:#?}", status.lock().unwrap().jobs_completed);
         thread::sleep(Duration::from_millis(500));
